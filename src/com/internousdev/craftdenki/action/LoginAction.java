8527f829ff7cdc27f9cea885b72c818ac4f30251
Takahashiyuuri <a33a070e@gmail.com>
2018-02-06 13:52:39 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/action/LoginAction.java
+++ src/com/internousdev/craftdenki/action/LoginAction.java
@@ -1,6 +1,8 @@
 package com.internousdev.craftdenki.action;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.struts2.interceptor.SessionAware;
 
@@ -20,6 +22,39 @@ public class LoginAction extends ActionSupport implements SessionAware {
 
 	public String execute() {
 		result = ERROR;
+
+//IDについて
+        Pattern p1 = Pattern.compile("^[0-9a-zA-Z]*$");
+        Matcher m1 = p1.matcher(loginId);
+
+        if(m1.find() == false ){
+        	String errormsg1 = "IDは半角英数字で入力してください";
+        }
+
+		int length = loginId.getBytes().length;
+        if(length < 1) { // 最小文字数よりも少なかった場合
+            String IdErrorMsg = "IDは1文字以上8文字以内で入力してください";
+        }else if(length > 8) { // 最大文字数よりも多かった場合
+            String IdErrorMsg = "IDは1文字以上8文字以内で入力してください";
+        }else if(length == 0) { // 文字数が0だった場合
+            String IdErrorMsg = "IDは1文字以上8文字以内で入力してください";
+        }
+
+
+
+//Passについて
+        Pattern p2 = Pattern.compile("^[0-9a-zA-Z]*$");
+        Matcher m2 = p2.matcher(loginPassword);
+
+        if(m2.find() == false){
+        	String errormsg1 = "パスワードは半角英数字で入力してください";
+        }
+
+        System.out.println(m2.find());
+
+
+
+
 
 		dto = dao.loginUserInfo(loginId, loginPassword);
 
