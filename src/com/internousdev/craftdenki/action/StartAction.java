9baff8eb0fe52ca598888c7cb48d2f3514dd53d4
carldesu01 <carldesu01@gmail.com>
2018-02-07 13:12:59 +0900

    struts更新しました(sano)

---
+++ src/com/internousdev/craftdenki/action/StartAction.java
@@ -0,0 +1,11 @@
+package com.internousdev.craftdenki.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StartAction extends ActionSupport{
+
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
