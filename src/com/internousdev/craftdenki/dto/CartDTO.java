43a5867769aa0fbfc6a831c3e682e23497d8932d
enna331 <citenna331@gmail.com>
2018-02-07 09:54:00 +0900

    CartDTO更新します(塩治)

---
+++ src/com/internousdev/craftdenki/dto/CartDTO.java
@@ -0,0 +1,69 @@
+package com.internousdev.craftdenki.dto;
+
+public class CartDTO {
+
+
+	private int productId;
+	private String productName;
+	private String productNameKana;
+	private String imageFile;
+	private int price;
+	private int productCount;
+	private String releaseCompany;
+	//発売年月日が未記入
+	private int total_count;
+
+
+
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductNameKana() {
+		return productNameKana;
+	}
+	public void setProductNameKana(String productNameKana) {
+		this.productNameKana = productNameKana;
+	}
+	public String getImageFile() {
+		return imageFile;
+	}
+	public void setImageFile(String imageFile) {
+		this.imageFile = imageFile;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getProductCount() {
+		return productCount;
+	}
+	public void setProductCount(int productCount) {
+		this.productCount = productCount;
+	}
+	public String getReleaseCompany() {
+		return releaseCompany;
+	}
+	public void setReleaseCompany(String releaseCompany) {
+		this.releaseCompany = releaseCompany;
+	}
+	public int getTotal_count() {
+		return total_count;
+	}
+	public void setTotal_count(int total_count) {
+		this.total_count = total_count;
+	}
+
+
+
+}
