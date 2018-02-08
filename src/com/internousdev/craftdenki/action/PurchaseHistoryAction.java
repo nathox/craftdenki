d3812c67cb404c3d36f7f3ce4f0d9666302ea57d
okada-ayame <una.k@i.softbank.jp>
2018-02-08 16:38:28 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/action/PurchaseHistoryAction.java
+++ src/com/internousdev/craftdenki/action/PurchaseHistoryAction.java
@@ -22,26 +22,28 @@ public class PurchaseHistoryAction extends ActionSupport implements SessionAware
 	private String deleteFlg = null;
 	private String userId;
 	private String Message;
-
-
+	private String product_id;
+	private String user_id;
+	private String delete_id;
 
 	public String execute() throws SQLException {
+		String userId = session.get("trueID").toString();
+		System.out.println(userId);
 
 		if (!session.containsKey("trueID")) {
 			return ERROR;
 		}
 
 		// セッションからログインIDを取得する
-		String userId = session.get("loginId").toString();
 
 		// 購入履歴検索処理
 		if (this.deleteFlg == null || this.deleteFlg.isEmpty()) {
 			// ログインID(userID)を利用して、DAOから情報を検索する
 			purchaceHistoryList = purchaseHistoryDAO.getPurchaseHistory(userId);
 
-			Iterator<PurchaseHistoryDTO> iterator =  purchaceHistoryList.iterator();
-			if(!(iterator.hasNext())){
-				 purchaceHistoryList = null;
+			Iterator<PurchaseHistoryDTO> iterator = purchaceHistoryList.iterator();
+			if (!(iterator.hasNext())) {
+				purchaceHistoryList = null;
 			}
 
 		}
@@ -51,38 +53,33 @@ public class PurchaseHistoryAction extends ActionSupport implements SessionAware
 	}
 
 	@Override
-	public void setSession(Map<String, Object> session){
+	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
 
-	public String getuserId(){
+	public String getuserId() {
 		return userId;
 	}
-	public void setuserId(String userId){
+
+	public void setuserId(String userId) {
 		this.userId = userId;
 
-
 	}
 
-	public void delete(delete_id) throws SQLException{
-
-
-	String userId = session.get("loginId").toString();
-
-	int res = PurchaseHistoryDAO.deleteHistory(product_id,user_id,delete_id)
-
-	if(res>0){
-		purchaseHistoryList=null;
-		setMessage("購入履歴を削除しました");
-	}else if(res == 0){
-		setMessage("削除に失敗しました");
-	}
+	public String delete() throws SQLException {
+		String userId = session.get("loginId").toString();
+		int res = purchaseHistoryDAO.deleteHistory(product_id, user_id, delete_id);
+		String setMessage = null;
+		if (res > 0) {
+			setMessage("購入履歴を削除しました");
+			return setMessage;
+		} else{
+			setMessage("削除に失敗しました");
+			return setMessage;
+		}
 	}
 
 	private void setMessage(String string) {
-
-	}
-
 
 	}
 
