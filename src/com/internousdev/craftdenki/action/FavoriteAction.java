e2068f965dc3691d6320b70a22d7434e143b8969
nathox <ntex17.th@icloud.com>
2018-02-08 18:25:01 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/action/FavoriteAction.java
+++ src/com/internousdev/craftdenki/action/FavoriteAction.java
@@ -2,6 +2,8 @@ package com.internousdev.craftdenki.action;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
@@ -12,23 +14,39 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class FavoriteAction extends ActionSupport implements SessionAware{
 
-	private FavoriteDAO FavoriteDAO = new FavoriteDAO();
-	private ArrayList<FavoriteDTO> favoriteList = new ArrayList<FavoriteDTO>();
+	private List<FavoriteDTO> favoriteList = new ArrayList<FavoriteDTO>();
 	private String deleteFlg;
 	private String message;
 	public Map<String, Object> session;
-	private String result;
+
+	private Collection<String> checkList;
 
 	private String test = "test";
 
 	public String execute() throws SQLException {
+
+		String result=ERROR;
+
+		FavoriteDAO dao = new FavoriteDAO();
+
+		int count = 0;
+
+
+
 		if (deleteFlg == null) {
-			favoriteList = FavoriteDAO.getMyPageUserInfo(test);
+			favoriteList = dao.getMyPageUserInfo(test);
 
 
 			System.out.println(favoriteList);
 
-		} else if (deleteFlg.equals("1")) {
+		} else if (deleteFlg == "1") {
+
+			for(String product_id : checkList){
+				count += dao.deleteFavoriteInfo(product_id);
+				System.out.println("aaa");
+			}
+
+			System.out.println("削除された件数　：" + count);
 
 			// for(型 変数名 :コレクション)でコレクションの値をひとつずつ取り出し{}内を実行
 			// for(String deleteId:delete){
@@ -40,6 +58,9 @@ public class FavoriteAction extends ActionSupport implements SessionAware{
 		result = SUCCESS;
 		return result;
 	}
+
+
+
 
 	// public void delete(int deleteId) throws SQLException{
 	// String item_transaction_id = session1.get("id").toString();
@@ -55,13 +76,35 @@ public class FavoriteAction extends ActionSupport implements SessionAware{
 	// }
 	// }
 
-	public ArrayList<FavoriteDTO> getFavoriteList() {
+
+
+
+	public Collection<String> getCheckList() {
+		return checkList;
+	}
+
+
+
+
+	public List<FavoriteDTO> getFavoriteList() {
 		return favoriteList;
 	}
 
-	public void setFavoriteList(ArrayList<FavoriteDTO> favoriteList) {
+
+
+
+	public void setFavoriteList(List<FavoriteDTO> favoriteList) {
 		this.favoriteList = favoriteList;
 	}
+
+
+
+
+	public void setCheckList(Collection<String> checkList) {
+		this.checkList = checkList;
+	}
+
+
 
 
 
