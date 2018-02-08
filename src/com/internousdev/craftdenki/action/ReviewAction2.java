571b50c6d5340cd0814c7c8286ab1fe3aaf135be
Takahashiyuuri <a33a070e@gmail.com>
2018-02-08 15:10:59 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/action/ReviewAction2.java
+++ src/com/internousdev/craftdenki/action/ReviewAction2.java
@@ -5,28 +5,57 @@ import java.util.Map;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.craftdenki.dao.Review2DAO;
-import com.internousdev.craftdenki.dto.Review2DTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ReviewAction2 extends ActionSupport implements SessionAware {
 
 	private String review;
-	private String historyItemName;
+	private String productId;
+	private String buyItemDate;
+	private String evaluationCount;
 	private String result;
 	private Review2DAO dao = new Review2DAO();
-	private Review2DTO dto = new Review2DTO();
 	public Map<String,Object>session;
 
 	public String execute(){
 		result = SUCCESS;
 
-		dto = dao.getReviewInfo(historyItemName,review);
-
-
-
-
+		dao.getReviewInfo(session.get("trueID").toString(),review,productId,buyItemDate,evaluationCount);
 
 		return result;
+	}
+
+	public String getReview() {
+	    return review;
+	}
+
+	public void setReview(String review) {
+	    this.review = review;
+	}
+
+	public String getProductId() {
+	    return productId;
+	}
+
+	public void setProductId(String productId) {
+	    this.productId = productId;
+	}
+
+	public String getBuyItemDate() {
+	    return buyItemDate;
+	}
+
+	public void setBuyItemDate(String buyItemDate) {
+	    this.buyItemDate = buyItemDate;
+	}
+
+
+	public String getEvaluationCount() {
+	    return evaluationCount;
+	}
+
+	public void setEvaluationCount(String evaluationCount) {
+	    this.evaluationCount = evaluationCount;
 	}
 
 	@Override
