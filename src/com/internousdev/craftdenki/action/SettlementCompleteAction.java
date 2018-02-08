815f8e5086bddb15bd60ee1dfe42f6f48c4e89ee
carldesu01 <carldesu01@gmail.com>
2018-02-08 16:05:17 +0900

    struts,決済機能更新します(sano

---
+++ src/com/internousdev/craftdenki/action/SettlementCompleteAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.craftdenki.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class SettlementCompleteAction extends ActionSupport {
+
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
