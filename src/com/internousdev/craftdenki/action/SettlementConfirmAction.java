01a397229e4249b63c46cbdf781401cc1e41f293
carldesu01 <carldesu01@gmail.com>
2018-02-08 18:29:18 +0900

    更新します(sano

--- src/com/internousdev/craftdenki/action/SettlementConfirmAction.java
+++ src/com/internousdev/craftdenki/action/SettlementConfirmAction.java
@@ -5,8 +5,27 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class SettlementConfirmAction extends ActionSupport {
 
+	private String destination;
+
+
 	public String execute(){
+
+		System.out.println(destination);
+		
+
+
+
+
 		return SUCCESS;
 	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
 
 }
