3870183365da21ad285193a00ea969e611ede534
internousdev <internousdev@192.168.1.72>
2018-02-09 11:11:40 +0900

    product_infoテーブルのstatusが0のもののみリストを取得するように変更　岡本

--- src/com/internousdev/craftdenki/dao/ProductListDAO.java
+++ src/com/internousdev/craftdenki/dao/ProductListDAO.java
@@ -16,10 +16,11 @@ public class ProductListDAO {
 	private Connection connection = dbConnector.getConnection();
 
 	//商品リスト情報の取得
+	//ユーザーに表示するリスト
 	public ArrayList<ProductDTO> getProductInfo() throws SQLException{
 		ArrayList<ProductDTO> productList = new ArrayList<ProductDTO>();
 
-		String sql = "SELECT * FROM product_info ORDER BY Product_id ASC";
+		String sql = "SELECT * FROM product_info WHERE status = 0 ORDER BY Product_id ASC";
 
 	try{
 		PreparedStatement statement = connection.prepareStatement(sql);
@@ -40,7 +41,46 @@ public class ProductListDAO {
 			dto.setImage_file_name(resultSet.getString("image_file_name"));
 			dto.setRelease_date(resultSet.getString("release_date"));
 			dto.setRelease_company(resultSet.getString("release_company"));
-			dto.setStatus(resultSet.getInt("status"));
+			dto.setRegist_date(resultSet.getDate("regist_date"));
+			dto.setUpdate_date(resultSet.getDate("update_date"));
+			dto.setItem_stock(resultSet.getInt("item_stock"));
+			productList.add(dto);
+
+		}
+	}catch(Exception e){
+		e.printStackTrace();
+	}finally{
+		connection.close();
+	}
+		return productList;
+	}
+
+	//商品リスト情報の取得
+	//ユーザーに非表示のリスト
+	public ArrayList<ProductDTO> getProductHideInfo() throws SQLException{
+		ArrayList<ProductDTO> productList = new ArrayList<ProductDTO>();
+
+		String sql = "SELECT * FROM product_info WHERE status = 0 ORDER BY Product_id ASC";
+
+	try{
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		ResultSet resultSet = statement.executeQuery();
+
+		while(resultSet.next()){
+			ProductDTO dto = new ProductDTO();
+
+			dto.setId(resultSet.getInt("id"));
+			dto.setProduct_id(resultSet.getInt("product_id"));
+			dto.setProduct_name(resultSet.getString("product_name"));
+			dto.setProduct_name_kana(resultSet.getString("product_name_kana"));
+			dto.setProduct_description(resultSet.getString("product_description"));
+			dto.setCategory_id(resultSet.getInt("category_id"));
+			dto.setPrice(resultSet.getInt("price"));
+			dto.setImage_file_path(resultSet.getString("image_file_path"));
+			dto.setImage_file_name(resultSet.getString("image_file_name"));
+			dto.setRelease_date(resultSet.getString("release_date"));
+			dto.setRelease_company(resultSet.getString("release_company"));
 			dto.setRegist_date(resultSet.getDate("regist_date"));
 			dto.setUpdate_date(resultSet.getDate("update_date"));
 			dto.setItem_stock(resultSet.getInt("item_stock"));
