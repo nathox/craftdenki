9d98a6e24931ad3f8f206322d95fa07ef14b00ef
carldesu01 <carldesu01@gmail.com>
2018-02-13 14:04:14 +0900

    StartAction更新(sano

--- src/com/internousdev/craftdenki/action/StartAction.java
+++ src/com/internousdev/craftdenki/action/StartAction.java
@@ -13,18 +13,27 @@ public class StartAction extends ActionSupport implements SessionAware {
 
 	public String execute() {
 
-		if (!(session.containsKey("trueID"))) {
-			session.clear();
-		}
 
-		if (!(session.containsKey("trueId") || (session.containsKey("temp_user_id")))) {
 
+
+		if(!(session.containsKey("trueID"))){
+			System.out.println("本ログインしていない");
+			if(!(session.containsKey("temp_user_id"))){
+				System.out.println("仮持ってない");
 				Random rnd = new Random();
-
+				session.put("temp_user_id", rnd);
 				System.out.println("ランダム" + rnd + " = ランダムID");
 				System.out.println("仮ユーザーID発行");
-				session.put("temp_user_id", rnd);
+
+			}else{
+				System.out.println("仮ユーザーIDでログイン");
+			}
+		}else{
+			System.out.println("本ユーザーでログインしています");
 		}
+
+
+
 
 		return SUCCESS;
 	}
