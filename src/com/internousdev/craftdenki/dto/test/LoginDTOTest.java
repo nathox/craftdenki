876229e29672abc745d22a4751f12f26a9a5eafd
Takahashiyuuri <a33a070e@gmail.com>
2018-02-19 17:37:15 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/dto/test/LoginDTOTest.java
+++ src/com/internousdev/craftdenki/dto/test/LoginDTOTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.internousdev.craftdenki.dto.CategoryDTO;
 import com.internousdev.craftdenki.dto.LoginDTO;
 
 public class LoginDTOTest {
@@ -21,69 +20,8 @@ public class LoginDTOTest {
 
 	@Test
 	public void testGetLoginId2() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = null;
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-	}
-
-	@Test
-	public void testGetLoginId3() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = "";
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-	}
-
-	@Test
-	public void testGetLoginId4() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = " 　";
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-
-	}
-
-	@Test
-	public void testGetLoginId5() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = "aaaa";
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-	}
-
-	@Test
-	public void testGetLoginId6() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = "AAAA";
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-	}
-
-	@Test
-	public void testGetLoginId7() {
-		CategoryDTO dto = new CategoryDTO();
-		String expected = "ああああ";
-
-		dto.setId(expected);
-
-		assertEquals(expected, dto.getId());
-	}
-
-	@Test
-	public void testGetLoginPass1() {
 		LoginDTO dto = new LoginDTO();
-		String expected = "0";
+		String expected = null;
 
 		dto.setLoginId(expected);
 
@@ -91,63 +29,124 @@ public class LoginDTOTest {
 	}
 
 	@Test
+	public void testGetLoginId3() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+	}
+
+	@Test
+	public void testGetLoginId4() {
+		LoginDTO dto = new LoginDTO();
+		String expected = " 　";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+
+	}
+
+	@Test
+	public void testGetLoginId5() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "aaaa";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+	}
+
+	@Test
+	public void testGetLoginId6() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "AAAA";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+	}
+
+	@Test
+	public void testGetLoginId7() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "ああああ";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+	}
+
+	@Test
+	public void testGetLoginPass1() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "0";
+
+		dto.setLoginPass(expected);
+
+		assertEquals(expected, dto.getLoginPass());
+	}
+
+	@Test
 	public void testGetLoginPass2() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = null;
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 	@Test
 	public void testGetLoginPass3() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = "";
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 	@Test
 	public void testGetLoginPass4() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = " 　";
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 	@Test
 	public void testGetLoginPass5() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = "aaaa";
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 	@Test
 	public void testGetLoginPass6() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = "AAAA";
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 	@Test
 	public void testGetLoginPass7() {
-		CategoryDTO dto = new CategoryDTO();
+		LoginDTO dto = new LoginDTO();
 		String expected = "ああああ";
 
-		dto.setId(expected);
+		dto.setLoginPass(expected);
 
-		assertEquals(expected, dto.getId());
+		assertEquals(expected, dto.getLoginPass());
 	}
 
 }
