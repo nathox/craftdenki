f1baca6888b59eb69a0a6a03fbb1f6ab1124c25c
Takahashiyuuri <a33a070e@gmail.com>
2018-02-19 17:05:52 +0900

    更新(高橋)

--- src/com/internousdev/craftdenki/dto/test/LoginDTOTest.java
+++ src/com/internousdev/craftdenki/dto/test/LoginDTOTest.java
@@ -36,18 +36,118 @@ public class LoginDTOTest {
 
 		dto.setId(expected);
 
-		assertEquals(expected, dto.getId());	}
+		assertEquals(expected, dto.getId());
+	}
 
 	@Test
 	public void testGetLoginId4() {
-		fail("まだ実装されていません");
-	}
+		CategoryDTO dto = new CategoryDTO();
+		String expected = " 　";
 
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+
+	}
 
 	@Test
-	public void testGetLoginPass() {
-		fail("まだ実装されていません");
+	public void testGetLoginId5() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "aaaa";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
 	}
 
+	@Test
+	public void testGetLoginId6() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "AAAA";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginId7() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "ああああ";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass1() {
+		LoginDTO dto = new LoginDTO();
+		String expected = "0";
+
+		dto.setLoginId(expected);
+
+		assertEquals(expected, dto.getLoginId());
+	}
+
+	@Test
+	public void testGetLoginPass2() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = null;
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass3() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass4() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = " 　";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass5() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "aaaa";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass6() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "AAAA";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
+
+	@Test
+	public void testGetLoginPass7() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "ああああ";
+
+		dto.setId(expected);
+
+		assertEquals(expected, dto.getId());
+	}
 
 }
