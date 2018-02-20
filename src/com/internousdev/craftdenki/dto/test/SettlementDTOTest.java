145af378105f833a25ddb766272d9fbd5f009635
carldesu01 <carldesu01@gmail.com>
2018-02-20 15:22:18 +0900

    test更新（さの

---
+++ src/com/internousdev/craftdenki/dto/test/SettlementDTOTest.java
@@ -0,0 +1,64 @@
+package com.internousdev.craftdenki.dto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SettlementDTOTest {
+
+	@Test
+	public void testGetFamilyName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetFirstName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetUserAddress() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetUserAddress2() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetUserAddress3() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetTelNumber() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetTelNumber2() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetTelNumber3() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetAtCost() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetImageFilePath() {
+		fail("まだ実装されていません");
+	}
+
+}
